7eedc28de31c66399b6b35b6d8056279db5835cb
URK21CS1101 <110010779+URK21CS1101@users.noreply.github.com>
2022-11-04 18:10:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,62 +1,52 @@
 class BinaryTreeNode:
-    def __init__(self, data):
+	def __init__(self, data):
 self.data = data
 self.left_child = None
 self.right_child = None
-
-
-def insert(root, new_value) ->BinaryTreeNode:    
-    if not root:
-        root = BinaryTreeNode(new_value)
-        return root
-    if new_value<root.data:
-         if root.left_child:
+def insert(root, new_value) ->BinaryTreeNode:   
+	if not root:
+    	root = BinaryTreeNode(new_value)
+    	return root
+	if new_value<root.data:
+     	if root.left_child:
 insert(root.left_child, new_value)
-         else:
+     	else:
 root.left_child = BinaryTreeNode(new_value)
-    else:
-        if root.right_child:
+	else:
+    	if root.right_child:
 insert(root.right_child, new_value)
-        else:
+    	else:
 root.right_child = BinaryTreeNode(new_value)
-
-
 def inorder(root) -> None:
-    if root is None:
-        return
+	if root is None:
+    	return
 inorder(root.left_child)
 print(root.data, end = " ")
 inorder(root.right_child)
-
-
 def preorder(root) -> None:
-    if root is None:
-        return
+	if root is None:
+    	return
 print(root.data, end = " ")
-    preorder(root.left_child)
-    preorder(root.right_child)
-
-
+	preorder(root.left_child)
+	preorder(root.right_child)
 def postorder(root) -> None:
-    if root is None:
-        return
-    postorder(root.left_child)
-    postorder(root.right_child)
+	if root is None:
+    	return
+	postorder(root.left_child)
+	postorder(root.right_child)
 print(root.data, end = " ")
-
-
 # Do not change the following code
 input_data = input()
 flag = True
 root = None
 for item in input_data.split(', '):
-    if flag is True:
-        root = insert(None, int(item))
+	if flag is True:
+    	root = insert(None, int(item))
         flag = False
-    else:
+	else:
 insert(root, int(item))
 inorder(root)
 print()
-preorder(root) 
+preorder(root)
 print()
 postorder(root)
